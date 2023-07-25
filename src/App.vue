396f4ac2b1efbf10a10bf662367cf594c02bdf44
Courage OmniBarry <135624085+AndreOmniBarry@users.noreply.github.com>
2023-07-25 15:09:19 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <PopulationData />
+  </div>
+</template>
+
+<script>
+import PopulationData from './components/PopulationData.vue';
+
+export default {
+  components: {
+    PopulationData
+  }
+};
+</script>
+
+<style>
+.button {
+  background-color: #3c3b6e;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #c8102e;
+}
+</style>
